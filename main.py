08a6fc8c58b535e5f8ad65f90c006fc8fcf0d215
MichaelVerdi <43189162+MichaelVerdi@users.noreply.github.com>
2020-06-11 16:16:20 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+import socket, googlesearch, os
+
+def menu():
+    choice = print("What would you like to dork for?")
+
+
+def dork_craft_1():
+
+
+    return query
+
+def dork_craft_2():
+    return query
+
+def dork_craft_3():
+    return query
+
+def dork_craft_4():
+    return query
+
+def dork_craft_5():
+    return query
+
+def dork_craft_6():
+    return query
+
+def dork_craft_7():
+    return query
+
+def dork_craft_8():
+    return query
+
+
+
+def google_connection(query):
+    try:
+        dork = search(query, tld='com', lang='en', num=10, start=0, stop=35, pause=2.0)
+    except SearchError:
+        print("No Search Possible")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
